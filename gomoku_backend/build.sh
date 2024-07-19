a70eff47fe05d3ea0fd2ac8489f1c283ecbbcb63
Viet281101 <nva281101@gmail.com>
2024-07-19 05:21:15 +0200

    Update build.sh to add logs and check for successful migrations

--- gomoku_backend/build.sh
+++ gomoku_backend/build.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-set -e
-
-echo "Installing dependencies..."
+# Install dependencies
 pip install -r requirements.txt
 
+# Run migrations
 echo "Running migrations..."
 python manage.py migrate
 
+# Check if migrations ran successfully
+if [ $? -ne 0 ]; then
+	echo "Migrations failed. Exiting."
+	exit 1
+fi
+
+# Create default users
 echo "Creating default users..."
 python manage.py create_default_users
 
+# Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-echo "Build completed"
+echo "Build script completed successfully."
